7e3f48188d09bcc461abff28f7e4f3757e0d48ce
Marcelo <marcelorafaeldebrito@gmail.com>
2018-07-28 03:48:11 -0300

    First commit

---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	// "strings"
+	// "bytes"
+)
+
+func main() {
+	// http.HandleFunc("/", indexRoute)
+	currDir, _ := os.Getwd()
+	http.ErrNoLocation(indexRoute)
+	http.Handle("/", http.FileServer(http.Dir(currDir)))
+	http.ListenAndServe(":1234", nil)
+}
+func indexRoute(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(res, "Hello bitches")
+}
